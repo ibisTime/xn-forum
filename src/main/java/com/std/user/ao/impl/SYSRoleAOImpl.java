ea3a2de2fee7f83db1ffdcb3ff2851f831853ad6
pyez1158 <pyez1158@163.com>
2016-06-15 14:53:49 +0800

    解决1、删除角色，关联删除菜单

--- src/main/java/com/std/user/ao/impl/SYSRoleAOImpl.java
+++ src/main/java/com/std/user/ao/impl/SYSRoleAOImpl.java
@@ -2,13 +2,18 @@ package com.std.user.ao.impl;
 
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.std.user.ao.ISYSRoleAO;
+import com.std.user.bo.ISYSMenuRoleBO;
 import com.std.user.bo.ISYSRoleBO;
+import com.std.user.bo.IUserBO;
 import com.std.user.bo.base.Paginable;
 import com.std.user.domain.SYSRole;
+import com.std.user.domain.User;
 import com.std.user.exception.BizException;
 
 @Service
@@ -16,6 +21,12 @@ public class SYSRoleAOImpl implements ISYSRoleAO {
 
     @Autowired
     ISYSRoleBO sysRoleBO;
+
+    @Autowired
+    IUserBO userBO;
+
+    @Autowired
+    ISYSMenuRoleBO sysMenuRoleBO;
 
     @Override
     public String addSYSRole(SYSRole data) {
@@ -28,11 +39,20 @@ public class SYSRoleAOImpl implements ISYSRoleAO {
     }
 
     @Override
+    @Transactional
     public boolean dropSYSRole(String roleCode) {
         if (!sysRoleBO.isSYSRoleExist(roleCode)) {
             throw new BizException("lh4000", "角色编号不存在！");
         }
+        User condition = new User();
+        condition.setRoleCode(roleCode);
+        List<User> list = userBO.queryUserList(condition);
+        if (!CollectionUtils.sizeIsEmpty(list)) {
+            throw new BizException("lh4000", "该角色已在使用，无法删除！");
+        }
+        // 删除角色和角色菜单关联表
         sysRoleBO.removeSYSRole(roleCode);
+        sysMenuRoleBO.removeSYSMenuList(roleCode);
         return true;
     }
 
