c0286f7577194a9cdfae71ec6c3ce203425e1258
pyez1158 <pyez1158@163.com>
2016-10-26 23:16:28 +0800

    脚本更新

--- docs/truncate/truncate_xn_forum.sql
+++
@@ -1,5 +0,0 @@
-truncate tforum_site;
-truncate tforum_plate;
-truncate tforum_post;
-truncate tforum_comment;
-truncate tforum_post_talk;
---
+++ docs/truncate/truncate_xnforum.sql
@@ -0,0 +1,7 @@
+truncate tforum_comment;
+truncate tforum_keyword;
+truncate tforum_plate;
+truncate tforum_post;
+truncate tforum_post_talk;
+truncate tforum_prod_order;
+truncate tforum_product;
